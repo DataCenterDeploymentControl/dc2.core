4e0c3dc1509021122cd4b034de7d41f9612eb65e
Stephan Adig <sh@sourcecode.de>
2015-06-07 18:51:51 +0200

    Whatever

---
+++ migrations/versions/31596c3c1cc_.py
@@ -0,0 +1,22 @@
+"""empty message
+
+Revision ID: 31596c3c1cc
+Revises: 3c77a66b6ab
+Create Date: 2015-06-07 17:57:31.306406
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '31596c3c1cc'
+down_revision = '3c77a66b6ab'
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
